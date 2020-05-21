590f65e5ea0fad1eb0be313d71323e55dba33ac1
johndesu090 <37970753+johndesu090@users.noreply.github.com>
2020-05-21 10:24:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ include "$_SERVER[DOCUMENT_ROOT]/CloudFlare/Zone/Dns.php";
 
 $result = "";
 
-$key = "Your Cloudflare Zone ID goes here"; 
+$key = "d32277ecaaddc0da420885a0f85908c9"; 
 // Above Cloudflare Zone ID, find it in Domain Overview --> Domain Summary --> Zone ID
-$id = new \Cloudflare\Api("Your Cloudflare Email", "Your Cloudflare Global API Key");
+$id = new \Cloudflare\Api("exodia090@gmail.com", "60a16923235a0556c48b037fcd33b39a0d0dfy");
 // Above Cloudflare Email + Cloudflare Global API Key (https://www.cloudflare.com/a/profile) --> Global API Key
 $dns = new \Cloudflare\Zone\Dns($id);
 
 if(!empty($_POST["name"]) and !empty($_POST["value"]) and !empty($_POST["record"])) {
-    $response = $dns->create($key, $_POST["record"], $_POST["name"] . ".yourdomain.name", $_POST["value"], 1);
-    // Make sure to enter your domain name above (.yourdomain.name), or else the script won't work
+    $response = $dns->create($key, $_POST["record"], $_POST["name"] . ".dnsford.ml", $_POST["value"], 1);
+    // Make sure to enter your domain name above (.dnsford.ml), or else the script won't work
     if ($response->success) {
-        $result = '<div class="toast toast-success" style="margin: 0 auto; width:714px;text-align: center;"><b>Success!</b> Your hostname <b>' . $_POST['name'] . '.yourdomain.name</b> is now online!</div>';
+        $result = '<div class="toast toast-success" style="margin: 0 auto; width:714px;text-align: center;"><b>Success!</b> Your hostname <b>' . $_POST['name'] . '.dnsford.ml</b> is now online!</div>';
     } else {
-        $result = '<div class="toast toast-error" style="margin: 0 auto; width:714px;text-align: center;"><b>Sorry!</b> Your hostname <b>' . $_POST['name'] . '.yourdomain.name</b> could not be created!</div>';
+        $result = '<div class="toast toast-error" style="margin: 0 auto; width:714px;text-align: center;"><b>Sorry!</b> Your hostname <b>' . $_POST['name'] . '.dnsford.ml</b> could not be created!</div>';
     }
     
 }
@@ -26,7 +26,7 @@ if(!empty($_POST["name"]) and !empty($_POST["value"]) and !empty($_POST["record"
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>cloudflare-dns-creator</title>
+  <title>JohnFordTV DNS Maker</title>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1" name="viewport">
   <link href="/assets/spectre.min.css" rel="stylesheet">
@@ -44,16 +44,16 @@ if(!empty($_POST["name"]) and !empty($_POST["value"]) and !empty($_POST["record"
         <div class="card centered text-center">
           <div class="card-header">
             <div class="card-title h5">
-              cloudflare-dns-creator
+              JohnFordTV DNS Maker
             </div>
             <div class="card-subtitle text-gray">
-              Create your free <!-- might enter your domain name here --> domain now!
+              Create your dnsford.ml dns now!
             </div>
           </div>
           <div class="card-body">
             <form action="index.php" method="post">
               <div class="input-group">
-                <input class="form-input input-lg" name="name" pattern="^[a-zA-Z0-9]+$" placeholder="Hostname" required="" type="text"> <span class="input-group-addon addon-lg">.mcpe.ga</span>
+                <input class="form-input input-lg" name="name" pattern="^[a-zA-Z0-9]+$" placeholder="Hostname" required="" type="text"> <span class="input-group-addon addon-lg">.dnsford.ml</span>
               </div><br>
               <div class="input-group">
                 <input class="form-input input-lg" name="value" placeholder="IPv4 Address or Domain Name" required="" type="text">
